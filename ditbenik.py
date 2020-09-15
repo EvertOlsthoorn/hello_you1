ffcc3d5c6e1714fffc82be2cdc6fc7a081f9ea9d
EvertOlsthoorn <70955088+EvertOlsthoorn@users.noreply.github.com>
2020-09-15 15:30:05 +0200

    Add files via upload

---
+++ ditbenik.py
@@ -0,0 +1,18 @@
+print ("Hello you, ik ben Evert Olsthoorn")
+print ("Wat is u naam?")
+
+username = input("")
+print("Mijn naam is " + username)
+
+import datetime
+x = datetime.datetime.now()
+
+print(x)
+
+awnser = input("Enter yes or no: ")
+if answer == "yes":
+    print("YOURE THE MOST AWESOME PERSON TO HAVE EVER EXISTED!")
+elif answer == "no":
+    print("PISS OFF!")
+else:
+    print("Please enter yes or no")
